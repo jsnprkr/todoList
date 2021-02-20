9267f83120f19aff94f697594b3a29f2ea626a5c
Jason Parker <jsnprkr2@gmail.com>
2021-02-20 12:27:28 -0600

    Update App.js
    Added state tracking to the todos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todoText, setTodoText] = useState('');
+  const addTodo = () => {
+    console.log('Button Click')
+    
+  };
+  const onTextInput = (event) => {
+    setTodoText(event.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type = 'text' value={todoText} onInput={onTextInput}/>
+      <button onClick={addTodo}> add to todo list</button>
     </div>
   );
 }
